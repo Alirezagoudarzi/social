7cb9fecf9406bcbbfa643b33d71dfa3b3718a2d2
Alirezagoudarzi <arg_7ty7@yahoo.com>
2024-09-02 12:55:28 +0330

    register and login and logout complate

--- account/forms.py
+++ account/forms.py
@@ -7,6 +7,7 @@ class UserRegistrationForm(forms.Form):
     username=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
     email=forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control'}))
     password=forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    confirmpassword=forms.CharField(label='Confirm Password' , widget=forms.PasswordInput(attrs={'class':'form-control'}))
 
     def clean_email(self):
         email=self.cleaned_data['email']
@@ -26,4 +27,16 @@ class UserRegistrationForm(forms.Form):
         return username
 
 
+    def clean(self):
+        cd = super().clean() # اون سه تا تابع ارزیابی رو صدا میزنه و کلین دیتا رو میسازه
+        p1=cd.get('password')
+        p2=cd.get('confirmpassword')
+
+        if p1 and p2 and p1 != p2:
+            raise ValidationError('Password and confirm password must be mach!')
+        
+        
+class UserLoginForm(forms.Form):
+    username=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control'}))
+    password=forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control'}))
 
