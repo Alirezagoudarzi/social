8054f0c0f65ad368466e356e49d8218467c06bb0
Alirezagoudarzi <arg_7ty7@yahoo.com>
2024-09-04 21:28:36 +0330

    Post sorted by created time

--- home/models.py
+++ home/models.py
@@ -11,7 +11,9 @@ class Post(models.Model):
     slug=models.SlugField()
     created=models.DateTimeField(auto_now_add=True)
     updated=models.DateTimeField(auto_now=True)
-
+    
+    class Meta:
+        ordering=('-created',)
 
     def __str__(self) -> str:
         return self.slug
